80a72717b39a9b21cd804b4e5cd0dc9952399f83
zhangheng-max <3311951458@qq.com>
2019-09-18 09:55:01 +0800

    上传打包文件

---
+++ precache-manifest.8587e0d18047ebcdadbb20e884b68eeb.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = [
+  {
+    "revision": "08cec041676d118a478c",
+    "url": "js/Pronfile_info_address_add.29a8914e.js"
+  },
+  {
+    "revision": "c4670c7d7f64d0fcb5d6c696f82c1498",
+    "url": "menu.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "9a16965fa0fdfd804d92",
+    "url": "js/Pronfile_info_address_addDetail.261a6d00.js"
+  },
+  {
+    "revision": "2c2fac23733cd27c3328",
+    "url": "js/Download.1b91913c.js"
+  },
+  {
+    "revision": "2efb75ebdfb7a4c51fff",
+    "url": "js/Forget.41b258fc.js"
+  },
+  {
+    "revision": "4c8192468908928b4bf6",
+    "url": "js/cs.3c9ee985.js"
+  },
+  {
+    "revision": "f1d2d247dc0709473c4f",
+    "url": "js/Home.7aeea321.js"
+  },
+  {
+    "revision": "a5e0877298611f70c5fb",
+    "url": "js/confirmOrder.c9c825b7.js"
+  },
+  {
+    "revision": "55412c8be1d489988e3c",
+    "url": "js/Login.fc1f23fe.js"
+  },
+  {
+    "revision": "eb2f675484a1d4063042",
+    "url": "js/chunk-vendors.72804567.js"
+  },
+  {
+    "revision": "9f890741b43ad3f22fab",
+    "url": "js/Msite.97f5eecd.js"
+  },
+  {
+    "revision": "b9784ff2fad1fd7f4a40",
+    "url": "js/app.f9c46697.js"
+  },
+  {
+    "revision": "70ccaa0048d85d2a21ad",
+    "url": "js/Order.d853d2b4.js"
+  },
+  {
+    "revision": "d36bef0c327ab22b7817",
+    "url": "js/Shop.9652482d.js"
+  },
+  {
+    "revision": "b3f9d0f0d532b238df44",
+    "url": "js/Profile.bf345863.js"
+  },
+  {
+    "revision": "80006880ead049c0bdad",
+    "url": "js/Search.eabec246.js"
+  },
+  {
+    "revision": "0541c382ee64c0056f82",
+    "url": "js/Profile_info.632dca29.js"
+  },
+  {
+    "revision": "2a95d6d5209074b4f369",
+    "url": "js/Pronfile_info_setusername.696006f8.js"
+  },
+  {
+    "revision": "509b66249b034bb3791d",
+    "url": "js/Pronfile_info_address.df4851d5.js"
+  },
+  {
+    "revision": "70ccaa0048d85d2a21ad",
+    "url": "css/Order.b12aea5f.css"
+  },
+  {
+    "revision": "08cec041676d118a478c",
+    "url": "css/Pronfile_info_address_add.236428a1.css"
+  },
+  {
+    "revision": "9a16965fa0fdfd804d92",
+    "url": "css/Pronfile_info_address_addDetail.4356dbca.css"
+  },
+  {
+    "revision": "90182c92e4c7d7210ae3405bd6bb7b30",
+    "url": "img.gif"
+  },
+  {
+    "revision": "2a95d6d5209074b4f369",
+    "url": "css/Pronfile_info_setusername.1e9f154d.css"
+  },
+  {
+    "revision": "69f7afec6ac052606166012bc600bf9d",
+    "url": "index.html"
+  },
+  {
+    "revision": "80006880ead049c0bdad",
+    "url": "css/Search.b84a2bec.css"
+  },
+  {
+    "revision": "90182c92e4c7d7210ae3405bd6bb7b30",
+    "url": "img/img.90182c92.gif"
+  },
+  {
+    "revision": "d36bef0c327ab22b7817",
+    "url": "css/Shop.c8a951e8.css"
+  },
+  {
+    "revision": "4c8192468908928b4bf6",
+    "url": "css/cs.89c8098d.css"
+  },
+  {
+    "revision": "b9784ff2fad1fd7f4a40",
+    "url": "css/app.0fc92f2a.css"
+  },
+  {
+    "revision": "5fd84622cd90faaad2c5",
+    "url": "js/City.3c328247.js"
+  },
+  {
+    "revision": "eb2f675484a1d4063042",
+    "url": "css/chunk-vendors.ae141bf9.css"
+  },
+  {
+    "revision": "a5e0877298611f70c5fb",
+    "url": "css/confirmOrder.42df065a.css"
+  },
+  {
+    "revision": "509b66249b034bb3791d",
+    "url": "css/Pronfile_info_address.279c4a01.css"
+  },
+  {
+    "revision": "0541c382ee64c0056f82",
+    "url": "css/Profile_info.e95f85a3.css"
+  },
+  {
+    "revision": "b3f9d0f0d532b238df44",
+    "url": "css/Profile.34364bb3.css"
+  },
+  {
+    "revision": "9f890741b43ad3f22fab",
+    "url": "css/Msite.db9fddbd.css"
+  },
+  {
+    "revision": "55412c8be1d489988e3c",
+    "url": "css/Login.39790c93.css"
+  },
+  {
+    "revision": "f1d2d247dc0709473c4f",
+    "url": "css/Home.f5ff6fa1.css"
+  },
+  {
+    "revision": "2efb75ebdfb7a4c51fff",
+    "url": "css/Forget.3a757d3f.css"
+  },
+  {
+    "revision": "2c2fac23733cd27c3328",
+    "url": "css/Download.5112aa1c.css"
+  },
+  {
+    "revision": "5fd84622cd90faaad2c5",
+    "url": "css/City.9e347b94.css"
+  }
+];
